10e92f7606d6d0b8bd19c75c6416d9ed8402bd73
keside <wilsonezeala@yahoo.com>
2023-03-18 19:58:15 +0100

    chore: add default style for header and p tag

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,9 @@
 
 <template>
   <main>
-    <h1 class="tracking-1">SPARTAN</h1>
+    <h1 class="">Aliquam</h1>
+    <h2 class="">Aliquam</h2>
+    <h3 class="">Aliquam</h3>
+    <p class="">Aliquam</p>
   </main>
 </template>
