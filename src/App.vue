38c0bd86235276579b697593d37af23c8c31561a
keside <wilsonezeala@yahoo.com>
2023-03-23 08:51:33 +0100

    complete UI for custom checkbox

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@ const themeStore = useThemeStore()
     >
       <header><AppNavbar /></header>
 
-      <main class="max-w-[730px] mx-6 lg:mx-auto border-gray-100/10 border">
+      <main class="max-w-[730px] mx-6 md:mx-auto border-gray-100/10 border">
         <RouterView />
       </main>
     </div>
--- src/components/form/InputCheckbox.vue
+++ src/components/form/InputCheckbox.vue
@@ -1,13 +1,48 @@
 <template>
-  <label class="flex items-center gap-3 cursor-pointer">
+  <!-- @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)" -->
+  <label class="flex items-center gap-3 capitalize cursor-pointer">
     <input
       type="checkbox"
-      name=""
+      :id="props.label"
+      v-model="model"
+      :value="props.label"
+      :checked="props.checked"
       class="relative grid w-4 h-4 transition-all bg-gray-200 border border-[transparent] rounded-sm appearance-none cursor-pointer dark:bg-blue-300 hover:border-purple-300 hover:border-opacity-100 dark:checked:bg-purple-300 checked:bg-purple-300 checked:border-purple-300 place-items-center"
     />
-    checkbox
+    {{ props.label }}
   </label>
 </template>
+
+<script setup lang="ts">
+import { computed, defineProps } from 'vue'
+
+const emit = defineEmits(['update:modelValue'])
+
+const model = computed({
+  get() {
+    return props.modelValue
+  },
+
+  set(value) {
+    emit('update:modelValue', value)
+  }
+})
+const props = defineProps({
+  modelValue: {
+    type: Array
+  },
+  label: {
+    required: true,
+    type: String,
+    default: 'checkbox'
+  },
+  checked: {
+    required: false,
+    type: Boolean,
+    default: false
+  }
+})
+</script>
 
 <style>
 input[type='checkbox']:checked {
