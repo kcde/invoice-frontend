975b300fc0b91ce6e84d53c3ad8fc5de762e2db7
keside <wilsonezeala@yahoo.com>
2023-04-09 23:21:11 +0100

    fix: make form span full width

---
+++ src/components/form/BaseInput.vue
@@ -0,0 +1,26 @@
+<template>
+  <label class="relative block">
+    <p
+      class="text-sm text-purple-100 capitalize dark:text-gray-200 mb-[10px]"
+      :class="{ 'sr-only': !showLabel }"
+    >
+      {{ label }}
+    </p>
+    <slot></slot>
+  </label>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  label: {
+    type: String,
+    required: true
+  },
+
+  showLabel: {
+    type: Boolean,
+    required: false,
+    default: true
+  }
+})
+</script>
--- src/components/form/InputText.vue
+++ src/components/form/InputText.vue
@@ -1,11 +1,5 @@
 <template>
-  <label class="relative block">
-    <p
-      class="text-sm text-purple-100 capitalize dark:text-gray-200 mb-[10px]"
-      :class="{ 'sr-only': !showLabel }"
-    >
-      {{ label }}
-    </p>
+  <BaseInput :label="label">
     <input
       type="text"
       name=""
@@ -14,10 +8,12 @@
       class="w-full py-4 pl-5 text-sm font-bold text-blue-500 transition-colors duration-200 border border-gray-200 rounded dark:text-white dark:border-blue-200 leading-sm focus:border-purple-200 dark:focus:border-purple-200 focus:outline-none placeholder:text-blue-500/30 dark:bg-blue-300 caret-purple-300"
       :placeholder="placeholder"
     />
-  </label>
+  </BaseInput>
 </template>
 
 <script lang="ts" setup>
+import BaseInput from './BaseInput.vue'
+
 defineProps({
   label: {
     type: String,
