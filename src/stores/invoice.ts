775d3f3fecf0bbb6414004736d2d3740b4fdeb83
keside <wilsonezeala@yahoo.com>
2023-06-16 12:05:57 +0100

    remove invoice store local storage persistence

--- src/stores/invoice.ts
+++ src/stores/invoice.ts
@@ -2,32 +2,24 @@ import type { IInvoicePayload, IInvoice } from '@/types'
 import { defineStore } from 'pinia'
 import { computed, ref, type Ref } from 'vue'
 
-export const useInvoiceStore = defineStore(
-  'invoice',
-  () => {
-    const invoices: Ref<IInvoice[]> = ref([])
+export const useInvoiceStore = defineStore('invoice', () => {
+  const invoices: Ref<IInvoice[]> = ref([])
 
-    const invoiceCount = computed(() => {
-      return invoices.value.length
-    })
+  const invoiceCount = computed(() => {
+    return invoices.value.length
+  })
 
-    function addInvoice(invoice: IInvoice) {
-      invoices.value.push(invoice)
-    }
-
-    function setInvoice(invoicesArray: IInvoice[]) {
-      invoices.value = invoicesArray
-    }
-
-    function resetInvoiceStore() {
-      invoices.value = []
-    }
-
-    return { invoices, addInvoice, invoiceCount, resetInvoiceStore, setInvoice }
-  },
-  {
-    persist: {
-      key: 'invoice-app-invoice-store'
-    }
+  function addInvoice(invoice: IInvoice) {
+    invoices.value.push(invoice)
   }
-)
+
+  function setInvoice(invoicesArray: IInvoice[]) {
+    invoices.value = invoicesArray
+  }
+
+  function resetInvoiceStore() {
+    invoices.value = []
+  }
+
+  return { invoices, addInvoice, invoiceCount, resetInvoiceStore, setInvoice }
+})
