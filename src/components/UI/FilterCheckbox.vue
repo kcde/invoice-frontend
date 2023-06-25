010cf4e706910bbbfa010698298495293d7feb0b
keside <wilsonezeala@yahoo.com>
2023-06-25 22:48:56 +0100

    WIP: clear invoice filter

--- src/components/UI/FilterCheckbox.vue
+++ src/components/UI/FilterCheckbox.vue
@@ -14,19 +14,25 @@
     </button>
 
     <div
-      class="absolute bg-white dark:bg-blue-200 rounded-lg top-[calc(100%+1.5rem)] min-w-[192px] left-2/4 -translate-x-2/4 shadow-xl shadow-[#48549F40] duration-300 dark:shadow-blue-500 py-6 pl-6 space-y-4"
+      class="absolute bg-white dark:bg-blue-200 rounded-lg top-[calc(100%+1.5rem)] min-w-[192px] left-2/4 -translate-x-2/4 shadow-xl shadow-[#48549F40] duration-300 dark:shadow-blue-500 py-6 pl-6 z-10"
       role="listbox"
       v-if="isCheckListOpen"
     >
-      <InputCheckRadio
-        v-for="filter in filters"
-        :key="filter.name"
-        :label="filter.name"
-        v-model="selectedFilter"
-        type="radio"
-        name="invoice-filter"
-        :checked="filter.name == selectedFilter"
-      />
+      <div class="absolute top-2 right-4" @click="invoiceStore.resetInvoiceFilter">
+        <ClearFilterIcon />
+      </div>
+
+      <div class="space-y-4">
+        <InputCheckRadio
+          v-for="filter in filters"
+          :key="filter.name"
+          :label="filter.name"
+          v-model="selectedFilter"
+          type="radio"
+          name="invoice-filter"
+          :checked="filter.name == invoiceStore.invoiceFilter"
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -37,12 +43,16 @@ import CaretIcon from '../icons/CaretIcon.vue'
 import { onClickOutside } from '@vueuse/core'
 import { ref, watch } from 'vue'
 import InputCheckRadio from '../form/InputCheckRadio.vue'
+import ClearFilterIcon from '../icons/ClearFilterIcon.vue'
+import { useInvoiceStore } from '@/stores/invoice'
 
 const isCheckListOpen = ref(false)
 const checkboxContainer = ref(null)
 const selectedFilter = ref('')
 
 const emit = defineEmits(['filter'])
+
+const invoiceStore = useInvoiceStore()
 const toggleCheckList = () => {
   isCheckListOpen.value = !isCheckListOpen.value
 }
