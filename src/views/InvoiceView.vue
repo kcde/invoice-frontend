0b360baaa20f034cecd59ac0ac8f981d102dbe45
keside <wilsonezeala@yahoo.com>
2023-07-11 21:57:34 +0100

    add back button on invoiceView page

--- src/views/InvoiceView.vue
+++ src/views/InvoiceView.vue
@@ -1,9 +1,22 @@
 <template>
   <div>
+    <div class="mb-8">
+      <button class="cursor-pointer" @click="$router.push('/')">
+        <div class="flex items-center gap-6 text-sm">
+          <div class="-rotate-90">
+            <CaretIcon />
+          </div>
+          Go back
+        </div>
+      </button>
+    </div>
+
     <h1>{{ $route.params.id }}</h1>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import CaretIcon from '@/components/icons/CaretIcon.vue'
+</script>
 
 <style scoped></style>
