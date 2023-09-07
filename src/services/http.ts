e4b56edae342640847c6343a77848bf22c5db38f
keside <wilsonezeala@yahoo.com>
2023-09-07 03:39:42 +0100

    update ip address

--- src/services/http.ts
+++ src/services/http.ts
@@ -4,7 +4,7 @@ const ENV = process.env.NODE_ENV
 let endpoint = 'https://localhost:1234/api/'
 
 if (ENV == 'production') {
-  endpoint = 'https://16.171.224.211:1234/api'
+  endpoint = 'https://16.16.215.95:1234/api'
 }
 
 const http = {
